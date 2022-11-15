50de320104834d6136665e93328256ea821584bb
shoulinniao <8898764+shoulinniao@user.noreply.gitee.com>
2022-11-16 03:13:21 +0800

    2022年11月16日每日一题

--- java/思维/LC775.java
+++ java/思维/LC775.java
@@ -5,7 +5,7 @@ public class LC775 {
     /**
      * 【局部倒置】一定是【全局倒置】，因此可以判断是否有【非局部倒置】即可
      * 是否存在nums[i]>nums[j]，j!=i+1。
-     * 取min(nums[i+2], min[i+3], ... min[n-1])即可
+     * 判断nums[i] > min(nums[i+2], nums[i+3], ... nums[n-1])，是否存在
      */
     public boolean isIdealPermutation(int[] nums) {
         int n = nums.length;
