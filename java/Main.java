b46c3ec69af1e822513ae88bf15a4445f33169d0
liyangjin <772013350@qq.com>
2025-07-07 11:27:41 +0800

    LC1353 贪心 + 最小堆

--- java/Main.java
+++ java/Main.java
@@ -10,40 +10,10 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    /**
-     * nums长度就100000，缺失的最大正数不会超过100001
-     * nums[i]范围在[1, n]才有意义，负数和大数可以忽略掉。
-     * 对于有效数组nums[i]用x表示
-     * nums[x] = -nums[x]，负数表示出现过的正数
-     * 如果原数组就是负数，由于它没有用，先随便赋值成大数
-     */
-    public static int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            //0也是没用的，别占用位置
-            if (nums[i] <= 0) {
-                nums[i] = n + 1;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            int x = Math.abs(nums[i]); //nums[2] 可能被 nums[0]=2的时候标记成负数
-            //n是边界值，也需要标记，把0干掉了都往前挪一位
-            if (x <= n) {
-                //这里为什么还需要用Math.abs，因为nums[x]可能已经被标记成负数了，可以有重复数字
-                nums[x - 1] = -Math.abs(nums[x - 1]);
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                return i + 1;
-            }
-        }
-        return n + 1;
-    }
+
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 0};
-        System.out.println(firstMissingPositive(nums));
     }
 
 }
---
+++ java/贪心/LC1353.java
@@ -0,0 +1,60 @@
+package 贪心;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * @author: liyangjin
+ * @create: 2025-07-07 11:23
+ * @Description:
+ */
+public class LC1353 {
+    /**
+     * 按结束时间排序死在这里，[[1,5],[1,5],[1,5],[2,3],[2,3]]
+     * 按开始时间排序死在这里，[[1,2],[1,2],[3,3],[1,5],[1,5]]
+     * 重新整理思路，对于第i天，可参加的会议有哪些？
+     * 有一个最小堆来维护当前可参加的会议的结束时间，先参加最早结束的会议。
+     */
+    public int maxEvents(int[][] events) {
+        Arrays.sort(events, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);  // 先按开始时间升序
+            } else {
+                return Integer.compare(a[1], b[1]);  // 再按结束时间升序
+            }
+        });
+        int max = 0;
+        for (int[] event : events) {
+            max = Math.max(max, event[1]);
+        }
+        //第i天开始的会议
+        List<Integer>[] groups = new ArrayList[max + 1];
+        Arrays.setAll(groups, i -> new ArrayList<>());
+        for (int[] event : events) {
+            groups[event[0]].add(event[1]);
+        }
+
+        int cnt = 0;
+        //维护可参加会议结束时间的最小堆
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int curDay = 1; curDay <= max; curDay++) {
+            //删除过期会议
+            while (!pq.isEmpty() && pq.peek() < curDay) {
+                pq.poll();
+            }
+            //新增可参加的会议
+            for (int endDay : groups[curDay]) {
+                pq.offer(endDay);
+            }
+            //参加一个结束时间最早的会议
+            if (!pq.isEmpty()) {
+                pq.poll();
+                cnt++;
+            }
+
+        }
+        return cnt;
+    }
+}
